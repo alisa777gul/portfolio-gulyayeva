147d4e84a5eb5bce8f0a4ab58f782853baa167fd
aliso4ka <gulyayevaalisa@gmail.com>
2025-02-27 22:23:00 +0100

    nn

---
+++ src/About/About.jsx
@@ -0,0 +1,17 @@
+import css from "./About.module.css";
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className={css.about}>
+      <h2 className={css.title}>About me</h2>I am a Junior Front-End Developer
+      skilled in HTML5, CSS3, JavaScript, and React. I am passionate about
+      creating userfriendly, modern web solutions and continually improving my
+      technical expertise. I adapt quickly to new environments, solve challenges
+      effectively, and thrive in team settings. My strengths include analytical
+      thinking, troubleshooting, and a proactive attitude that helps deliver
+      high-quality results. I value growth, teamwork, and contributing to
+      impactful projects.
+    </div>
+  );
+};
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Header/Header.jsx";
 import Hero from "./Hero/Hero.jsx";
+import { About } from "./About/About.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <About />
     </>
   );
 }
