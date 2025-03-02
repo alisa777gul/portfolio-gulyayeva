65bfee5e5aa719be901e41c50148747ef1760c2e
aliso4ka <gulyayevaalisa@gmail.com>
2025-03-02 18:06:35 +0100

    gg

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -14,32 +14,34 @@ export const Footer = () => {
       <p className={css.text}>Code. Create. Inspire.</p>
       <ul className={css.socials}>
         <li className={css.social}>
-          <a href="">
-            <img src="/ig.svg" />
+          <a href="https://www.instagram.com/frursnlshwumhrt/">
+            <img src="/ig.svg" alt="insta" />
           </a>
         </li>
         <li className={css.social}>
-          <a href="">
-            <img src="/tg.svg" />
+          <a href="https://t.me/iiikkklllm">
+            <img src="/tg.svg" alt="telegram" />
           </a>
         </li>
         <li className={css.social}>
-          <a href="">
-            <img src="/git.svg" />
+          <a href="https://github.com/alisa777gul">
+            <img src="/git.svg" alt="github" />
           </a>
         </li>
         <li className={css.social}>
-          <a href="">
-            <img src="/linked.svg" />
+          <a href="https://www.linkedin.com/in/alisa-gulyayeva/">
+            <img src="/linked.svg" alt="linkedin" />
           </a>
         </li>
       </ul>
 
       <div className={css.info}>
         <p className={css.location}>
-          <img src="/dot.svg" /> Presov
+          <img src="/dot.svg" alt="place" /> Slovakia
         </p>
-        <p className={css.rights}>&#169; 2025, All Rights By alisa777gul.</p>
+        <p className={css.rights}>
+          &#169; 2025, All Rights Reserved By alisa777gul.
+        </p>
       </div>
     </footer>
   );
