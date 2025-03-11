18dfbfbbc403bad2c24f1f7e7af88112ee628925
aliso4ka <gulyayevaalisa@gmail.com>
2025-03-11 17:04:13 +0100

    fixing

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import css from "./Footer.module.css";
+import icons from "../assets/sprite.svg";
 
 export const Footer = () => {
   return (
@@ -14,34 +15,58 @@ export const Footer = () => {
       <p className={css.text}>Code. Create. Inspire.</p>
       <ul className={css.socials}>
         <li className={css.social}>
-          <a href="https://www.instagram.com/frursnlshwumhrt/" target="_blank">
-            <img src="/ig.svg" alt="insta" loading="lazy" />
+          <a
+            href="https://www.instagram.com/frursnlshwumhrt/"
+            target="_blank"
+            className={css.link}
+          >
+            <svg className={css.icon}>
+              <use href={`${icons}#icon-instagram`} />
+            </svg>
           </a>
         </li>
         <li className={css.social}>
-          <a href="https://t.me/iiikkklllm" target="_blank">
-            <img src="/tg.svg" alt="telegram" loading="lazy" />
+          <a
+            href="https://t.me/iiikkklllm"
+            target="_blank"
+            className={css.link}
+          >
+            <svg className={css.icon}>
+              <use href={`${icons}#icon-telegram`} />
+            </svg>
           </a>
         </li>
         <li className={css.social}>
-          <a href="https://github.com/alisa777gul" target="_blank">
-            <img src="/git.svg" alt="github" loading="lazy" />
+          <a
+            href="https://github.com/alisa777gul"
+            target="_blank"
+            className={css.link}
+          >
+            <svg className={css.icon}>
+              <use href={`${icons}#icon-github`} />
+            </svg>
           </a>
         </li>
         <li className={css.social}>
           <a
             href="https://www.linkedin.com/in/alisa-gulyayeva/"
             target="_blank"
+            className={css.link}
           >
-            <img src="/linked.svg" alt="linkedin" loading="lazy" />
+            <svg className={css.icon}>
+              <use href={`${icons}#icon-linkedin`} />
+            </svg>
           </a>
         </li>
       </ul>
 
       <div className={css.info}>
-        <p className={css.location}>
-          <img src="/dot.svg" alt="place" loading="lazy" /> Slovakia
-        </p>
+        <div className={css.location}>
+          <svg className={css.pin} width={12} height={12}>
+            <use href={`${icons}#icon-location`} />
+          </svg>
+          <span> Slovakia</span>
+        </div>
         <p className={css.rights}>
           &#169; 2025, All Rights Reserved By alisa777gul.
         </p>
--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -5,7 +5,7 @@ export const Project = ({ project }) => {
   return (
     <div className={css.element}>
       <h3 className={css.title}>{project.name}</h3>
-      <p className={css.tech}>{project.tech}</p>
+      <p className={css.tech}>{project.tech.join(", ")}</p>
       <img src={project.photo} className={css.photo} loading="lazy" />
       <div className={css.links}>
         <a href={project.git} className={css.link} target="_blank">
