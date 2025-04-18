f51637d10d7506987b2564428ba190e7e1c333ef
aliso4ka <gulyayevaalisa@gmail.com>
2025-04-18 15:16:48 +0200

    optimized

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -4,7 +4,7 @@ import css from "./ProjectsList.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation"; // Include navigation styles
+import "swiper/css/navigation";
 import "swiper/css/effect-creative";
 import {
   Pagination,
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode, Suspense } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import "./i18n.js";
 import MainLoader from "./components/MainLoader/MainLoader.jsx";
+
+const App = lazy(() => import("./App.jsx"));
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
